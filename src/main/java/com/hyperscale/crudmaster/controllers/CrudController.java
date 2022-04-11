f0defde3ee2d534b1780151628d8cc5f983af0ba
mohinipasalkar <mohinipasalkar@gmail.com>
2022-04-11 16:51:49 +0200

    Initial commit

--- src/main/java/com/hyperscale/crudmaster/controllers/CrudController.java
+++ src/main/java/com/hyperscale/crudmaster/controllers/CrudController.java
@@ -3,32 +3,57 @@ package com.hyperscale.crudmaster.controllers;
 import com.hyperscale.crudmaster.models.Person;
 import com.hyperscale.crudmaster.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CrudController {
 
     @Autowired
-    private PersonRepository personRepository;
+    public PersonRepository personRepository;
 
     @GetMapping("/hello")
+    @ResponseStatus(HttpStatus.OK)
     public String helloTest(){
         return "Hello welcome to Hyperscale-developers";
     }
 
-    @PostMapping("/person")
+    @PostMapping("/persons")
+    @ResponseStatus(HttpStatus.CREATED)
     public String createPerson(@RequestBody Person p){
         Person newPerson = personRepository.saveAndFlush(p);
         return"Created person with id: "+newPerson.getId();
     }
 
     @GetMapping("/persons")
+    @ResponseStatus(HttpStatus.OK)
     public List<Person> showPersons(){
         return personRepository.findAll();
+    }
+
+    @GetMapping("/persons/{person_id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Person> showPerson(@PathVariable Long person_id){
+        return personRepository.findById(person_id);
+    }
+
+    @DeleteMapping("/persons/{person_id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deletePerson(@PathVariable("person_id") long pid){
+        personRepository.deleteById(pid);
+    }
+
+    @PutMapping("/persons/{person_id}")
+    public ResponseEntity<Person> updatePerson(@RequestBody Person p, @PathVariable long person_id){
+
+        Person per =  personRepository.saveAndFlush(p);
+        if(per == null){
+            ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(per);
     }
 }
